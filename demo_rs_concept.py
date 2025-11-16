a94a67cac6702fe7db04cb1abc26d7db3121329d
aaronalexander666 <aaronalexanderq4@gmail.com>
2025-11-15 22:16:02 -0500

    Create demo_rs_concept.py

---
+++ demo_rs_concept.py
@@ -0,0 +1,5 @@
+Original gradient: [ 0.1  -0.05  0.3  -0.2   0.15 -0.1   0.25 -0.15]
+...
+Recovered gradient: [0.59765625 0.296875   1.         0.         0.69921875 0.19921875 0.8984375  0.09765625]
+Recovery error: 1.330775  <-- **This is the problem**
+A recovery error of
